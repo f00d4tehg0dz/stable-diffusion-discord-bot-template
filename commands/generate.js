659f9cb41095da39f0e149e12fda4c010c660368
Adrian Chrysanthou <adrianvfx@gmail.com>
2023-01-09 15:54:36 -0500

    final build with advanced and generate commands

---
+++ commands/generate.js
@@ -0,0 +1,85 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const WebSocket = require('ws');
+const {
+  SlashCommandBuilder,
+  AttachmentBuilder
+} = require('discord.js');
+const createHash = require('hash-generator');
+
+
+function generateHash() {
+  let hash = createHash(12)
+  return {
+    session_hash: hash,
+    fn_index: 2
+  }
+}
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('generate')
+    .setDescription('Generates an image from a text prompt using Stable Diffusion 1.5')
+    .addStringOption(option => option
+      .setName('prompt')
+      .setDescription('generate image prompt')
+    ),
+  async execute(interaction) {
+
+    const prompt = interaction.options.getString('prompt');
+    console.log('What to generate?', prompt);
+
+    try {
+      await interaction.reply("I'm generating...");
+
+      const ws = new WebSocket('wss://runwayml-stable-diffusion-v1-5.hf.space/queue/join');
+      const hash = generateHash();
+      ws.on('open', () => {});
+
+      ws.on('message', async (message) => {
+        const msg = JSON.parse(`${message}`);
+        if (msg.msg === 'send_hash') {
+          ws.send(JSON.stringify(hash));
+        } else if (msg.msg === 'send_data') {
+          const data = {
+            data: [prompt],
+            ...hash,
+          };
+          ws.send(JSON.stringify(data));
+        } else if (msg.msg === 'process_completed') {
+          try {
+            const results = msg.output.data[0];
+            const attachments = [];
+            for (let i = 0; i < results.length; i++) {
+              const data = results[i].split(',')[1];
+              const buffer = Buffer.from(data, 'base64');
+              const attachment = new AttachmentBuilder(buffer, {
+                name: 'generate.png',
+              });
+              attachments.push(attachment);
+            }
+            interaction.editReply({
+              content: `You asked me for ${prompt}`,
+              files: attachments,
+            });
+          } catch (error) {
+            console.error(error);
+            await interaction.editReply({
+              content: 'An error occurred while generating the image',
+            });
+          }
+        }
+      });
+
+      ws.on('error', async (error) => {
+        console.error(error);
+        await interaction.editReply({
+          content: 'An error occurred while generating the image',
+        });
+      });
+    } catch (error) {
+      console.error(error);
+    }
+
+  },
+};
